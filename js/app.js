290ea167fecc445c1928bd1c65103e3b13ea8ea4
Aravindh. E <Aravindhmca2@gmail.com>
2021-12-29 21:51:49 +0530

    mypersonal-website

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active");
+    document.querySelector(".navbar").classList.toggle("show");
+  })
+})
+//Footer Year 
+var year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
